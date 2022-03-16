4b509d5a6ad73c86c34a09b1ee183c3a65b009a0
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-16 19:06:43 -0300

    fix: middleware login

--- src/middlewares/loginMiddleware.js
+++ src/middlewares/loginMiddleware.js
@@ -4,8 +4,7 @@ const loginMiddleware = async (req, res, next) => {
   const { email, password } = req.body;
 
   const user = await userService.getAllUsers();
-
-  if (user.some((usr) => usr.email !== email || usr.password !== password)) {
+  if (!user.some((usr) => usr.email === email || usr.password === password)) {
     return res.status(402).json({ message: 'Email ou senha inválidos' });
   }
 
